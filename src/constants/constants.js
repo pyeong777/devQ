cdb9fbf17560908b4e0a22e3becc9d2afbda0b2d
pyeong777 <seowns9520@gmail.com>
2023-05-30 17:11:18 +0900

    feat: 상수 값 추가

---
+++ src/constants/constants.js
@@ -0,0 +1,20 @@
+export const navmenu = [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "React",
+  "CS",
+  "자료구조",
+  "북마크",
+];
+
+export const path = [
+  "html",
+  "css",
+  "javascript",
+  "react",
+  "cs",
+  "ds",
+  "contact",
+  "bookmark",
+];
