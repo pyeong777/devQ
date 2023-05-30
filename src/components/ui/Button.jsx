afe97b66449e976cdf5675e1a963a809b39c5edc
pyeong777 <seowns9520@gmail.com>
2023-05-30 17:11:58 +0900

    feat: 버튼 UI 컴포넌트 추가

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,9 @@
+import styles from "./Button.module.css";
+
+export default function Button({ text, onClick }) {
+  return (
+    <button className={styles.button} onClick={onClick}>
+      {text}
+    </button>
+  );
+}
