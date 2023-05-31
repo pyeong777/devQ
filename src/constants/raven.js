58a219652f00333a55032e32400383dc48e9f177
pyeong777 <seowns9520@gmail.com>
2023-06-01 02:56:55 +0900

    ver 추가

--- src/constants/raven.js
+++ src/constants/raven.js
@@ -1,18 +1,22 @@
 export function raven() {
   console.log(
     `
+
         %c ██████╗ ███████╗██╗   ██╗ ██████╗ 
         %c ██╔══██╗██╔════╝██║   ██║██╔═══██╗
         %c ██║  ██║█████╗  ██║   ██║██║   ██║
         %c ██║  ██║██╔══╝  ╚██╗ ██╔╝██║▄▄ ██║
         %c ██████╔╝███████╗ ╚████╔╝ ╚██████╔╝
         %c ╚═════╝ ╚══════╝  ╚═══╝   ╚══▀▀═╝ 
+
+              %c Ver 1.00
         `,
     "color:#22577A",
     "color:#38A3A5",
     "color:#57CC99",
     "color:#80ED99",
     "color:#99FFED",
-    "color:#FFFFFF"
+    "color:#FFFFFF",
+    "color: black; font-size: 2rem"
   );
 }
