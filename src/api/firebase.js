8c5f0be0651fc2d31396a90a6c0d48373de8be28
pyeong777 <seowns9520@gmail.com>
2023-06-05 08:02:35 +0900

    feat: 데이터 호출 api 추가

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -7,6 +7,8 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 
+import { getDatabase, ref, set, get, remove } from "firebase/database";
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -15,6 +17,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
 
@@ -34,4 +37,73 @@ export function onUserStateChange(callback) {
   onAuthStateChanged(auth, (user) => {
     callback(user);
   });
+}
+
+export async function getHtmlList() {
+  return get(ref(database, "html")).then((snapshot) => {
+    if (snapshot.exists()) {
+      return Object.values(snapshot.val());
+    }
+    return [];
+  });
+}
+
+export async function getCssList() {
+  return get(ref(database, "css")).then((snapshot) => {
+    if (snapshot.exists()) {
+      return Object.values(snapshot.val());
+    }
+    return [];
+  });
+}
+
+export async function getJsList() {
+  return get(ref(database, "javascript")).then((snapshot) => {
+    if (snapshot.exists()) {
+      return Object.values(snapshot.val());
+    }
+    return [];
+  });
+}
+
+export async function getReactList() {
+  return get(ref(database, "react")).then((snapshot) => {
+    if (snapshot.exists()) {
+      return Object.values(snapshot.val());
+    }
+    return [];
+  });
+}
+
+export async function getCsList() {
+  return get(ref(database, "cs")).then((snapshot) => {
+    if (snapshot.exists()) {
+      return Object.values(snapshot.val());
+    }
+    return [];
+  });
+}
+
+export async function getBookMark(userId) {
+  return get(ref(database, `bookmark/${userId}`)).then((snapshot) => {
+    const items = snapshot.val() || {};
+    return Object.values(items);
+  });
+}
+
+export async function addToBookMark(userId, quest) {
+  return set(ref(database, `bookmark/${userId}/${quest.id}`), quest);
+}
+
+export async function removeFromBookMark(userId, questId) {
+  return remove(ref(database, `bookmark/${userId}/${questId}`));
+}
+
+export async function getIsBookMark(userId, questId) {
+  return get(ref(database, `bookmark/${userId}/${questId}`)).then(
+    (snapshot) => {
+      const items = snapshot.val() || {};
+      return Object.values(items);
+    }
+  );
 }
