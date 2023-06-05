8c557455b8f42fddff9472892475b8aabc00f4b1
pyeong777 <seowns9520@gmail.com>
2023-06-05 09:48:05 +0900

    fix typo

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -12,8 +12,8 @@ import { getDatabase, ref, set, get, remove } from "firebase/database";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
