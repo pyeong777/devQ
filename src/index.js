9e873409760970d3f8e047c21d1691f9b0217606
pyeong777 <seowns9520@gmail.com>
2023-06-05 09:28:24 +0900

    fix: 라우트 변경

--- src/index.js
+++ src/index.js
@@ -30,11 +30,7 @@ const router = createBrowserRouter([
       { path: "ds", element: <DataStructure /> },
       {
         path: "contact",
-        element: (
-          <ProtectedRoute>
-            <Contact />
-          </ProtectedRoute>
-        ),
+        element: <Contact />,
       },
       {
         path: "bookmark",
@@ -49,8 +45,4 @@ const router = createBrowserRouter([
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
